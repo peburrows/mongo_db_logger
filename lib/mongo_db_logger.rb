9a1c8046453e9682723394507fec2ac6dcc0fd80
phil <phil@882a0cfe-dafd-0310-a90d-f8c45283ee93>
2009-09-24 23:43:26 +0000

    the default should be not to log to MongoDB in development
    git-svn-id: https://svn.vitalbook.com/svn/plugins/trunk/mongo_db_logger@27837 882a0cfe-dafd-0310-a90d-f8c45283ee93

--- lib/mongo_db_logger.rb
+++ lib/mongo_db_logger.rb
@@ -6,7 +6,7 @@ module MongoDBLogger
   end
 
   def use_mongodb_logger
-    # return yield if (Rails.env == 'development')
+    return yield if (Rails.env == 'development')
     Rails.logger.mongoize(:action => action_name, :controller => controller_name, :params => params, :ip => request.remote_ip) { yield }
   end
 end
