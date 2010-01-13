38665356e25ea4f0e96dad8463594053694d71ad
phil <phil@882a0cfe-dafd-0310-a90d-f8c45283ee93>
2010-01-13 20:56:32 +0000

    record the full URL and the path of the request
    git-svn-id: https://svn.vitalbook.com/svn/plugins/trunk/mongo_db_logger@29913 882a0cfe-dafd-0310-a90d-f8c45283ee93

--- lib/mongo_db_logging.rb
+++ lib/mongo_db_logging.rb
@@ -8,6 +8,13 @@ module MongoDBLogging
     
     # make sure the controller knows how to filter its parameters
     f_params = respond_to?(:filter_parameters) ? filter_parameters(params) : params
-    Rails.logger.mongoize(:action => action_name, :controller => controller_name, :params => f_params, :ip => request.remote_ip) { yield }
+    Rails.logger.mongoize({
+      :action         => action_name,
+      :controller     => controller_name,
+      :path           => request.path,
+      :url            => request.url,
+      :params         => f_params,
+      :ip             => request.remote_ip
+    }) { yield }
   end
 end
