a577a998b9c8c7a5071b5df0842355e8c22cb335
Alex Stupka <astupka21@yahoo.com>
2010-09-27 00:55:15 -0400

    Used LogSubscriber to fix colorize_logging deprecation warning

--- lib/mongo_logger.rb
+++ lib/mongo_logger.rb
@@ -88,7 +88,7 @@ class MongoLogger < ActiveSupport::BufferedLogger
 
   def add(severity, message = nil, progname = nil, &block)
     unless @level > severity
-      if ActiveRecord::Base.colorize_logging
+      if (ActionPack::VERSION::MAJOR >= 3 ? ActiveRecord::LogSubscriber.colorize_logging : ActiveRecord::Base.colorize_logging)
         # remove colorization done by rails and just save the actual message
         @mongo_record[:messages][level_to_sym(severity)] << message.gsub(/(\e(\[([\d;]*[mz]?))?)?/, '').strip rescue nil
       else
