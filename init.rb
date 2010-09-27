ed6270657a0f221195fb535fa70e576c954aea4d
Alex Stupka <astupka21@yahoo.com>
2010-09-27 00:58:01 -0400

    Updated to support Rails 3's Plugin initializer architecture

--- init.rb
+++ init.rb
@@ -1,2 +1,12 @@
+# For Rails 3, this runs inside the Plugin initializer
 require 'mongo_logger'
+klass = if ActionPack::VERSION::MAJOR >= 3
+  ActionDispatch::Routing::DeprecatedMapper
+else
+  ActionController::Routing::RouteSet::Mapper
+end
+klass.extend MongoDBLogging::RoutingExtensions
+
+# runs after primary Bootstrap.rb log initializer
+# TODO: hook this in using a gem/Railtie to capture other boot log messages
 Rails.logger = config.logger = MongoLogger.new
