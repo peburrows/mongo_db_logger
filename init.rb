e4a5499dfc0b18d69a81b4fc234ab0d9bc458a06
Alex Stupka <astupka21@yahoo.com>
2010-09-27 12:54:45 -0400

    [Fixes #5375848] Used standard constants to test Rails version.

--- init.rb
+++ init.rb
@@ -1,12 +1,11 @@
 # For Rails 3, this runs inside the Plugin initializer
 require 'mongo_logger'
-klass = if ActionPack::VERSION::MAJOR >= 3
+klass = if Rails::VERSION::MAJOR >= 3
+  # runs after primary Bootstrap.rb log initializer
+  # TODO: hook this in using a gem/Railtie to capture other boot log messages
+  Rails.logger = config.logger = MongoLogger.new
   ActionDispatch::Routing::DeprecatedMapper
 else
   ActionController::Routing::RouteSet::Mapper
 end
 klass.extend MongoDBLogging::RoutingExtensions
-
-# runs after primary Bootstrap.rb log initializer
-# TODO: hook this in using a gem/Railtie to capture other boot log messages
-Rails.logger = config.logger = MongoLogger.new
