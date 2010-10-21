82e5b026c6123d5336830a0cf505c37d45a84ac1
Alex Stupka <astupka21@yahoo.com>
2010-10-21 12:11:56 -0400

    [Fixes #5801122] Moved helper methods to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,24 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 Shoulda.autoload_macros("#{File.dirname(__FILE__)}/..")
 
 class Test::Unit::TestCase
+  def log(msg)
+    @mongo_logger.mongoize({"id" => 1}) do
+      @mongo_logger.debug(msg)
+    end
+  end
+
+  def log_metadata(options)
+    @mongo_logger.mongoize({"id" => 1}) do
+      @mongo_logger.add_metadata(options)
+    end
+  end
+
+  def require_bogus_active_record
+    require 'active_record'
+  end
+
+  def common_setup
+    @con = @mongo_logger.mongo_connection
+    @collection = @con[@mongo_logger.mongo_collection_name]
+  end
 end
