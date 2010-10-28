2760ca5fbd4f2e11e251368619a907f52e24c9a2
Alex Stupka <astupka21@yahoo.com>
2010-10-28 12:58:31 -0400

    Removed controller and view code

--- lib/central_logger/routing_extensions.rb
+++
@@ -1,7 +0,0 @@
-module CentralLogger
-  module RoutingExtensions
-    def add_central_logger_resources!
-      resources :central_logger, :controller => "CentralLogger/Log", :only => [:show, :index, :destroy]
-    end
-  end
-end
